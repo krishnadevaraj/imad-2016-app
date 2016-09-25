965f791470eea4d0835398eccd7b9a0cecb644b3
krishnadevaraj <krishnadevaraj25@gmail.com>
2016-09-25 12:11:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ var articleOne = {
             </p>`
 };
 
-function createTemplate (data){
+function createTemplate(data){
     var title = data.title;
     var heading = data.heading;
     var date = data.date;
